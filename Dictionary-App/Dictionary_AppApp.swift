81cbde090d73e7647ba2144ef34f9c38abef8955
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-10-09 11:36:32 -0400

    Initial Commit

---
+++ Dictionary-App/Dictionary_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dictionary_AppApp.swift
+//  Dictionary-App
+//
+//  Created by Victor Marquez on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Dictionary_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
