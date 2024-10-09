1a4aa8a68edbf38d013af0e8f9b84d2adbd8c428
Victor Marquez <72099625+victorsheepman@users.noreply.github.com>
2024-10-09 12:40:20 -0400

    connected to api

--- Dictionary-App/ContentView.swift
+++ Dictionary-App/ContentView.swift
@@ -7,15 +7,65 @@
 
 import SwiftUI
 
+enum NetworkError: Error {
+    case invalidURL
+    case requestFailed(Error)
+    case invalidResponse
+    case decodingError(Error)
+    case invalidData
+}
+
 struct ContentView: View {
+    
+    @State private var word: Word?
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
             Text("Hello, world!")
+            
         }
         .padding()
+        .task {
+            do {
+                word = try await getWord()
+                print(word ?? "")
+            } catch NetworkError.invalidData {
+                print("Error: Invalid Data")
+            } catch NetworkError.invalidURL {
+                print("Error: La URL es inválida.")
+            } catch NetworkError.invalidResponse {
+                print("Error: La respuesta del servidor no es válida.")
+            } catch {
+                print("Error: Unexpected")
+            }
+        }
+    }
+    
+    func getWord() async throws -> [WordModel] {
+        let endpoint = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+        
+        guard let url = URL(string: endpoint) else {
+            throw NetworkError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            
+            return try decoder.decode([WordModel].self, from: data)
+            
+        } catch {
+            throw NetworkError.invalidData
+        }
     }
 }
 
---
+++ Dictionary-App/WordModel.swift
@@ -0,0 +1,58 @@
+//
+//  WordModel.swift
+//  Dictionary-App
+//
+//  Created by Victor Marquez on 9/10/24.
+//
+
+import Foundation
+
+// MARK: - WordModel
+
+struct WordModel: Codable {
+    let word, phonetic: String?
+    let phonetics: [Phonetic]?
+    let origin: String?
+    let meanings: [Meaning]?
+}
+
+// MARK: - Meaning
+struct Meaning: Codable {
+    let partOfSpeech: String?
+    let definitions: [Definition]?
+}
+
+// MARK: - Definition
+struct Definition: Codable {
+    let definition, example: String?
+    let synonyms, antonyms: [JSONAny]?
+}
+
+// MARK: - Phonetic
+struct Phonetic: Codable {
+    let text, audio: String?
+}
+
+typealias Word = [WordModel]
+
+
+class JSONCodingKey: CodingKey {
+    let key: String
+
+    required init?(intValue: Int) {
+            return nil
+    }
+
+    required init?(stringValue: String) {
+            key = stringValue
+    }
+
+    var intValue: Int? {
+            return nil
+    }
+
+    var stringValue: String {
+            return key
+    }
+}
+
